0bfe74107323449d525c5186a02214b93f071431
Gov Yadiki <govardhan.reddy@gmail.com>
2019-08-28 12:01:56 -0400

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetRestaurantById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ Id = 1, Name = "Gov's Pizza", Location = "Hershey", Cuisine =Restaurant.CuisineType.Mexican},
+                new Restaurant{ Id = 2, Name = "Yashu's Fries", Location = "Enola",Cuisine =Restaurant.CuisineType.Mexican},
+                new Restaurant{ Id = 3, Name = "Pete's Biryani", Location = "Harrisburg",Cuisine =Restaurant.CuisineType.Indian},
+                new Restaurant{ Id = 4, Name = "Tims' Chai", Location = "Mechanicsburg",Cuisine =Restaurant.CuisineType.Italian},
+                new Restaurant{ Id = 5, Name = "Super Coffee", Location = "Carlisle",Cuisine =Restaurant.CuisineType.None}
+
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return  from r in restaurants
+                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                    orderby r.Name
+                    select r;
+        }
+
+        public Restaurant GetRestaurantById(int id = 0)
+        {
+            return restaurants.SingleOrDefault(i => i.Id == id);
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+
+            }
+            return restaurant;
+        }
+        public Restaurant Add(Restaurant newRestaurant)
+        {            
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            restaurants.Add(newRestaurant);
+            return newRestaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using static OdeToFood.Core.Restaurant;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public ActionResult OnGet(int? restaurantId)
+        {
+            if (restaurantId != null)
+                Restaurant = restaurantData.GetRestaurantById(restaurantId.Value);
+            else
+                Restaurant = new Restaurant { };
+
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
+            if (Restaurant == null)
+                return RedirectToPage("./NotFound");
+            return Page();
+
+        }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
+            }
+            if (Restaurant.Id > 0)
+                restaurantData.Update(Restaurant);
+            else
+                restaurantData.Add(Restaurant);
+
+            restaurantData.Commit();
+            TempData["Message"] = "Restaurant saved!";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+
+        }
+    }
+}
